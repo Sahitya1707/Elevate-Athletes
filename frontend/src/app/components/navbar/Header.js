c3de284b76ffbe1bf24c7eef09d7a27e3654cb4f
Sahitya hp <neupanesahitya1@gmail.com>
2024-08-01 12:35:28 -0400

    chckeing github connection

--- frontend/src/app/components/navbar/Header.js
+++ frontend/src/app/components/navbar/Header.js
@@ -30,6 +30,7 @@ const Header = () => {
           alt="Logo"
         />
         <span className="">Elevate Athletes</span>
+        GIthub connect problem
       </div>
       <ul className="flex items-center gap-x-7">
         {navList &&
