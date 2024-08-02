d039547dd79a868e0899380f2192f31a955b4c3b
Sahitya hp <neupanesahitya1@gmail.com>
2024-08-01 23:41:40 -0400

    creating login for backend developer

--- frontend/src/app/api/login/page.js
+++ frontend/src/app/api/login/page.js
@@ -1,7 +1,11 @@
 import React from "react";
 
 const login = () => {
-  return <div>login</div>;
+  return (
+    <seciton className="w-[100vw] h-[100vh] flex items-center justify-center">
+      <div>sahitya</div>
+    </seciton>
+  );
 };
 
 export default login;
--- frontend/src/app/developer/api/login/page.js
+++ frontend/src/app/developer/api/login/page.js
@@ -1,5 +1,20 @@
+import React from "react";
+
 const login = () => {
-  return <div>login</div>;
+  return (
+    <seciton className="w-[100vw] h-[100vh] flex items-center justify-center">
+      <div className="border-solid border-2 border-[black] p-2 shadow-lg">
+        <div className="flex flex-col">
+          <span>Email</span>
+          <input type="email" />
+        </div>
+        <div>
+          <span>Password</span>
+          <input type="password" />
+        </div>
+      </div>
+    </seciton>
+  );
 };
 
 export default login;
