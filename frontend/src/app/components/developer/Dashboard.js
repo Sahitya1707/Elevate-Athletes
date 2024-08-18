ec98bcd52d3b08a149ecf5663cee209c7c4304c8
Sahitya hp <neupanesahitya1@gmail.com>
2024-08-18 01:13:27 -0400

    started creating dashboard

--- frontend/src/app/components/developer/Dashboard.js
+++ frontend/src/app/components/developer/Dashboard.js
@@ -1,9 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
+
+import { MdDashboard } from "react-icons/md";
+import DashboardLogoIcon from "./DashboardLogoIcon";
 
 const Dashboard = () => {
+  const [activeItem, setActiveItem] = useState(0);
   return (
-    <div className="bg-[red] w-[15rem] h-[100vh]">
-      <div></div>
+    <div
+      className="bg-primary w-[17rem] h-[100vh] py-5 px-3 border-textColor drop-shadow-3xl text-2xl"
+      style={{
+        boxShadow: "#0000007d 4px 0px 15px",
+      }}
+    >
+      <DashboardLogoIcon logo="/logo.png" text={"Elevate Athletes"} />
+      <div className="mx-auto my-4">
+        <div
+          className={`flex items-center gap-x-2  my-2 w-[90%] mx-auto rounded-md px-2 py-1 bg-secondary cursor-pointer tracking-wide`}
+        >
+          <span className="text-3xl">
+            <MdDashboard />
+          </span>
+          <span>Dashboard</span>
+        </div>
+      </div>
     </div>
   );
 };
---
+++ frontend/src/app/components/developer/DashboardLogoIcon.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Image from "next/image";
+
+const DashboardLogoIcon = ({ logo, text }) => {
+  return (
+    <div className="flex flex-col items-center">
+      <Image src={logo} width={40} height={40} className="" />
+      <span className="mt-2 uppercase text-2xl tracking-wider ">{text}</span>
+    </div>
+  );
+};
+
+export default DashboardLogoIcon;
