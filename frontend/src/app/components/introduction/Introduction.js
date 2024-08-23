944ac543a47567c5ac08a1207b31783c9952e908
Sahitya hp <neupanesahitya1@gmail.com>
2024-08-22 22:50:56 -0400

    added images

--- frontend/src/app/components/introduction/Introduction.js
+++ frontend/src/app/components/introduction/Introduction.js
@@ -1,7 +1,11 @@
 import React from "react";
 
 const Introduction = () => {
-  return <div className="h-[100vh] w-[100%]">Introduction</div>;
+  return (
+    <div className="h-[100vh] w-[100%] bg-primary relative">
+      <div></div>
+    </div>
+  );
 };
 
 export default Introduction;
