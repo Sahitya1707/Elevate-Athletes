ef9724b9a5c3c4ec5dc21d36d0843c95f08fd5de
Sahitya hp <neupanesahitya1@gmail.com>
2024-10-20 01:06:02 -0400

    cta made

---
+++ frontend/src/app/components/cta/Cta.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Cta = () => {
+  return <div className="h-[150vh] bg-[red]">CTA</div>;
+};
+
+export default Cta;
--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -3,6 +3,7 @@
 import FeaturePlayers from "./components/featuredPlayers/FeaturePlayers";
 import Container from "./components/hero/Container";
 import Introduction from "./components/introduction/Introduction";
+import Cta from "./components/cta/Cta";
 
 export default function Home() {
   return (
@@ -10,6 +11,7 @@ export default function Home() {
       <Container />
       <Introduction />
       <FeaturePlayers />
+      <Cta />
     </main>
   );
 }
