76ebaef27407ba46b2e03569432eae1d813ec560
Sahitya hp <neupanesahitya1@gmail.com>
2024-10-20 10:26:52 -0400

    upcoming events had been made

--- frontend/src/app/components/upcomingEvents/UpcomingEventCard.js
+++ frontend/src/app/components/upcomingEvents/UpcomingEventCard.js
@@ -1,39 +1,48 @@
 import React from "react";
 import Button from "../hero/Button";
 
-const UpcomingEventCard = () => {
+const UpcomingEventCard = ({
+  date,
+  heading,
+  organizer,
+  description,
+  eventLink,
+  virtualLink,
+}) => {
   return (
-    <div className="shadow-xl w-[25rem] p-4  text-left">
+    <div className="shadow-md w-[25rem] p-4  text-left border-2 border-solid border-primary">
       <div className="flex gap-x-4 justify-between">
-        <div className="w-[4rem] bg-primary h-[4rem] flex-col justify-center flex   text-revTextColor text-xl items-center shadow-xl uppercase">
+        <div className="w-[4rem] bg-primary h-[4rem] flex-col justify-center flex   text-revTextColor text-xl items-center shadow-lg uppercase">
+          {/* Date will be seperated and use it according as per the need */}
           <span>02</span>
           <span>Oct</span>
         </div>
         <span className="text-2xl w-[18rem] h-[5.5rem] text-wrap overflow-hidden font-bold uppercase">
-          Training Camps/ Wokshops sfdafadfadsf
+          {heading}
         </span>
       </div>
       <div className="border-t-2 border-b-2 border-solid border-tertiary py-4">
-        <p className="text-xl font-semibold">Organizer: HariHar Academy</p>
-        <p className="h-[7rem] overflow-hidden">
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Praesentium
-          doloremque possimus consequatur labore inventore. Ipsum iste non esse
-          culpa eaque dolorem et sequi distinctio cupiditate quod impedit,
+        <p className="text-xl font-semibold capitalize">
+          Organizer: {organizer}
         </p>
+        <p className="h-[7rem] overflow-hidden">{description}</p>
       </div>
       <div className="mt-4 flex gap-x-3">
         <Button
-          link={" "}
+          link={eventLink}
           text={"Learn More"}
           bgColor={"tertiary"}
           color={"textColor"}
         />
+        {/* After pressing booknow you will get the form popup and you should be available to book the events from there either virtually or in person. The virtual links props will be removed and whenever button is clicked popyp will be there. */}
+
         <Button
           link={""}
           text={"Book Now"}
           bgColor={"secondary"}
           color={"textColor"}
         />
+        {/* look the above comments */}
       </div>
     </div>
   );
--- frontend/src/app/components/upcomingEvents/UpcomingEvents.js
+++ frontend/src/app/components/upcomingEvents/UpcomingEvents.js
@@ -3,6 +3,7 @@ import SectionHeading from "../introduction/SectionHeading";
 import UpcomingEventCard from "./UpcomingEventCard";
 
 const UpcomingEvents = () => {
+  const fakeLoop = [1, 2, 3, 4, 5, 6, 7, 8];
   return (
     <div className="h-screen text-center py-6 px-[8rem]">
       <SectionHeading
@@ -10,8 +11,21 @@ const UpcomingEvents = () => {
         textColor={"tertiary"}
         textShadowColor={"primary"}
       />
-      <div className="mt-6">
-        <UpcomingEventCard />
+      <div className="mt-6 flex flex-wrap gap-y-16 gap-x-6 items-center justify-center">
+        {fakeLoop.map((e, i) => {
+          return (
+            <UpcomingEventCard
+              heading={"Traingin Camps / WorkShop"}
+              organizer={"Harihar Academy"}
+              description={
+                "     Lorem ipsum dolor, sit amet consectetur adipisicing elit. Praesentium   doloremque possimus consequatur labore inventore. Ipsum iste non esse culpa eaque dolorem et sequi distinctio cupiditate quod impedit,"
+              }
+              virtualLink={"/"}
+              key={i}
+              eventLink={"/"}
+            />
+          );
+        })}
       </div>
     </div>
   );
